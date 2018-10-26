e35365816f3e59ca32639295fa8b4777c1802233
TanyaZagor <42569857+TanyaZagor@users.noreply.github.com>
2018-10-27 01:21:15 +0300

    Lesson 3

--- StarGame/core/src/ru/geekbrains/stargame/screen/MenuScreen.java
+++ StarGame/core/src/ru/geekbrains/stargame/screen/MenuScreen.java
@@ -10,25 +10,23 @@ import ru.geekbrains.stargame.base.Base2DScreen;
 
 public class MenuScreen extends Base2DScreen {
 
-    private SpriteBatch batch;
-    private Texture background;
+
     private Texture img;
 
-    private Vector2 pos;
+    private Vector2 touchDownPos;
     private Vector2 v;
-    private Vector2 nextPos;
-    private float a; //acceleration
+    private Vector2 buf;
+    private Vector2 pos;
+
 
     @Override
     public void show() {
         super.show();
-        batch = new SpriteBatch();
-        background = new Texture("background.jpg");
-        img = new Texture("star.png");
+        img = new Texture("star2.jpg");
         pos = new Vector2(0, 0);
-        nextPos = new Vector2(0, 0);
-        v = new Vector2(0.5f, 0.3f);
-        a = 0.08f;
+        touchDownPos = new Vector2();
+        v = new Vector2();
+        buf = new Vector2();
     }
 
     @Override
@@ -36,49 +34,30 @@ public class MenuScreen extends Base2DScreen {
         super.render(delta);
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        buf.set(touchDownPos);
+        if (buf.sub(pos).len() > v.len()) {
+            pos.add(v);
+        } else {
+            pos.set(touchDownPos);
+        }
         batch.begin();
-        batch.draw(background, 0, 0);
-        batch.draw(img, pos.x, pos.y);
+        batch.draw(img,pos.x, pos.y, 0.05f, 0.05f);
         batch.end();
-        v = nextPos.cpy().sub(pos);
-        v.scl(a);
-        pos.add(v);
+
 
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
         img.dispose();
         super.dispose();
     }
 
     @Override
-    public boolean keyDown(int keycode) {
-        switch (keycode) {
-            case 19:
-                nextPos.set(pos.add(0, 20));
-                System.out.println("up");
-                break;
-            case 20:
-                nextPos.set(pos.add(0, -20));
-                System.out.println("down");
-                break;
-            case 21:
-                nextPos.set(pos.add(-20, 0));
-                System.out.println("left");
-                break;
-            case 22:
-                nextPos.set(pos.add(20, 0));
-                System.out.println("right");
-                break;
-        }
-        return super.keyDown(keycode);
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        nextPos.set(screenX,  Gdx.graphics.getHeight() - screenY);
-        return super.touchDown(screenX, screenY, pointer, button);
+    public boolean touchDown(Vector2 touch, int pointer) {
+        touchDownPos = touch;
+        v.set(touchDownPos.cpy().sub(pos).scl(0.01f));
+        return false;
     }
 }
