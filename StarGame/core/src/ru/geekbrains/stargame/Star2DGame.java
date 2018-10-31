2329809aec19b65092e30ad05bd01e2b3285dc9d
TanyaZagor <42569857+TanyaZagor@users.noreply.github.com>
2018-10-31 16:20:08 +0300

    Play (new screen)

--- StarGame/core/src/ru/geekbrains/stargame/Star2DGame.java
+++ StarGame/core/src/ru/geekbrains/stargame/Star2DGame.java
@@ -1,12 +1,18 @@
 package ru.geekbrains.stargame;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
 
+import ru.geekbrains.stargame.base.Base2DScreen;
 import ru.geekbrains.stargame.screen.MenuScreen;
 
 public class Star2DGame extends Game {
     @Override
     public void create() {
-        setScreen(new MenuScreen());
+        setScreen(new MenuScreen(this));
+    }
+
+    public void create(Base2DScreen screen) {
+        setScreen(screen);
     }
 }
--- StarGame/core/src/ru/geekbrains/stargame/screen/GameScreen.java
+++ StarGame/core/src/ru/geekbrains/stargame/screen/GameScreen.java
@@ -5,17 +5,13 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Vector2;
-
-import ru.geekbrains.stargame.Star2DGame;
 import ru.geekbrains.stargame.base.Base2DScreen;
 import ru.geekbrains.stargame.math.Rect;
 import ru.geekbrains.stargame.sprite.Background;
-import ru.geekbrains.stargame.sprite.Button;
 import ru.geekbrains.stargame.sprite.Star;
 
-public class MenuScreen extends Base2DScreen {
 
+public class GameScreen extends Base2DScreen {
     private static final int STAR_COUNT = 256;
 
     private Texture bgTexture;
@@ -23,16 +19,10 @@ public class MenuScreen extends Base2DScreen {
 
     private TextureAtlas textureAtlas;
     private Star[] stars;
-    private Button playButton;
-    private Button exitButton;
-    private Star2DGame star2DGame;
 
-    public MenuScreen(Star2DGame star2DGame) {
+    public GameScreen() {
         super();
-        this.star2DGame = star2DGame;
     }
-
-
     @Override
     public void show() {
         super.show();
@@ -43,8 +33,6 @@ public class MenuScreen extends Base2DScreen {
         for (int i= 0; i < stars.length; i++) {
             stars[i] = new Star(textureAtlas);
         }
-        playButton = new Button(textureAtlas, "btPlay", 0.3f, 0.2f);
-        exitButton = new Button(textureAtlas, "btExit", 0.2f, -0.2f);
 
 
     }
@@ -64,16 +52,12 @@ public class MenuScreen extends Base2DScreen {
         for (int i= 0; i < stars.length; i++) {
             stars[i].draw(batch);
         }
-        playButton.draw(batch);
-        exitButton.draw(batch);
         batch.end();
     }
     public void update(float delta) {
         for (int i= 0; i < stars.length; i++) {
             stars[i].update(delta);
         }
-        playButton.update(delta);
-        exitButton.update(delta);
     }
 
     @Override
@@ -91,27 +75,7 @@ public class MenuScreen extends Base2DScreen {
         super.dispose();
     }
 
-    @Override
-    public boolean touchDown(Vector2 touch, int pointer) {
-        if (touch.cpy().sub(playButton.pos).len() < 0.1) {
-            playButton.touchDown(touch, pointer);
-        }
-        if (touch.cpy().sub(exitButton.pos).len() < 0.1) {
-            exitButton.touchDown(touch, pointer);
-        }
-        return super.touchDown(touch, pointer);
-    }
 
-    @Override
-    public boolean touchUp(Vector2 touch, int pointer) {
-        if (touch.cpy().sub(playButton.pos).len() < 0.1) {
-            playButton.touchUp(touch, pointer);
-            star2DGame.create(new GameScreen());
-        }
-        if (touch.cpy().sub(exitButton.pos).len() < 0.1) {
-            exitButton.touchUp(touch, pointer);
-            Gdx.app.exit();
-        }
-        return super.touchUp(touch, pointer);
-    }
+
+
 }
